18272398dc0fef6d9d7b4e1faee24eed077f80ee
William Henlin <william.henlin@rangle.io>
2023-08-09 10:35:32 -0400

    Add table of services

---
+++ src/app/adf-services/df-service-table/df-service-table.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DfServiceTableComponent } from './df-service-table.component';
+
+describe('DfServiceTableComponent', () => {
+  let component: DfServiceTableComponent;
+  let fixture: ComponentFixture<DfServiceTableComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DfServiceTableComponent]
+    });
+    fixture = TestBed.createComponent(DfServiceTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/adf-services/df-service.module.ts
+++ src/app/adf-services/df-service.module.ts
@@ -4,13 +4,16 @@ import { DfServiceComponent } from './df-service/df-service.component';
 import { DfServiceDetailsComponent } from './df-service-details/df-service-details.component';
 import { DfServiceInfoComponent } from './df-service-info/df-service-info.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DfServiceTableComponent } from './df-service-table/df-service-table.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     DfServiceComponent,
     DfServiceDetailsComponent,
     DfServiceInfoComponent,
+    DfServiceTableComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, MatTableModule, ReactiveFormsModule],
 })
 export class DfServiceModule {}
