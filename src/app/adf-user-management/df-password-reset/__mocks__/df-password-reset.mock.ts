922884315cf06e425362580f72d84a02d2ff9cbe
Jasjeet Mavi <42419193+judgemavi@users.noreply.github.com>
2023-08-09 00:12:34 -0400

    remove dead code

--- src/app/adf-user-management/df-password-reset/__mocks__/df-password-reset.mock.ts
+++
@@ -1,8 +0,0 @@
-export const RESETFORMVALUE = {
-  email: 'test@example.com',
-  username: '',
-  code: '123456',
-  new_password: 'password',
-  verify_password: 'password',
-  bad_password: 'password123',
-};
