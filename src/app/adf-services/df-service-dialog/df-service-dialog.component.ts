5f8c0cf732e288a60f5db54a2724ca07977c9666
William Henlin <william.henlin@rangle.io>
2023-08-10 11:50:40 -0400

    Add stepper to create form

--- src/app/adf-services/df-service-dialog/df-service-dialog.component.ts
+++ src/app/adf-services/df-service-dialog/df-service-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,11 +8,21 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./df-service-dialog.component.scss'],
 })
 export class DfServiceDialogComponent {
+  isCreateServiceEnabled = true; // TODO: possibly change to input() when manage services component is being built
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
   constructor(
     public dialogRef: MatDialogRef<DfServiceDialogComponent>,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     // TODO: create dialog data object and remove the above eslint comment
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _formBuilder: FormBuilder
   ) {}
 
   onClose(): void {
