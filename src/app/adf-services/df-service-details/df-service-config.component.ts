a2bd94039bbc979d779ac447c26837bc5a179cda
William Henlin <william.henlin@rangle.io>
2023-08-10 20:18:26 -0400

    Add info and config section to services

---
+++ src/app/adf-services/df-service-details/df-service-config.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'df-service-config',
+  templateUrl: './df-service-config.component.html',
+  styleUrls: ['./df-service-config.component.scss'],
+})
+export class DfServiceConfigComponent {}
---
+++ src/app/adf-services/df-service-form/df-service-form.component.ts
@@ -0,0 +1,104 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+// TODO: remove above line
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export type ServiceSchema = {
+  id: number;
+  name: string;
+  label: string;
+  description: string;
+  isActive: boolean;
+  type: string;
+  config: object; // populated by secondFormGroup
+  service_doc_by_service_id: object | null; // populated by thirdFormGroup
+};
+
+@Component({
+  selector: 'df-service-form',
+  templateUrl: './df-service-form.component.html',
+  styleUrls: ['./df-service-form.component.scss'],
+})
+export class DfServiceFormComponent {
+  isCreateServiceEnabled = true; // TODO: change to input() so that this form can be toggled between create and manage
+  selectedSchema: ServiceSchema;
+
+  //Info
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: new FormControl('', Validators.required), // TODO: remove this ctrl when ready
+    namespace: new FormControl('', Validators.required),
+    label: new FormControl('', Validators.required),
+    description: new FormControl(''),
+    isActive: new FormControl(true),
+  });
+
+  //Config
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  //Definition
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+
+  constructor(
+    public dialogRef: MatDialogRef<DfServiceFormComponent>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // TODO: create dialog data object and remove the above eslint comment
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _formBuilder: FormBuilder
+  ) {
+    console.log('constructor');
+  }
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    return this.isCreateServiceEnabled
+      ? this.createService()
+      : this.updateService();
+  }
+
+  private createService(): void {}
+
+  private updateService(): void {}
+
+  showAdvancedFields(): void {}
+
+  hideAdvancedFields(): void {}
+
+  isFieldsSeparated(schemaName: string): boolean {
+    return (
+      schemaName === 'mysql' ||
+      schemaName === 'sqlsrv' ||
+      schemaName === 'oracle' ||
+      schemaName === 'pgsql'
+    );
+  }
+
+  isBasic(fieldName: string): boolean {
+    const basicFieldsNames = new Set([
+      'host',
+      'port',
+      'database',
+      'username',
+      'password',
+      'schema',
+    ]);
+
+    // mysql exception for schema field
+    if (this.selectedSchema.name === 'mysql' && fieldName === 'schema') {
+      return false;
+    }
+
+    return basicFieldsNames.has(fieldName);
+  }
+
+  isCaching(fieldName: string): boolean {
+    return fieldName.includes('cache') || fieldName.includes('caching');
+  }
+}
