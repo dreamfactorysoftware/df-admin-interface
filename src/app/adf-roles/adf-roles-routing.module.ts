52f255b680d9bc1312f4d0861c9b9a95f274a7fa
AnyaCraig <AnyaCraig@users.noreply.github.com>
2023-08-17 15:45:55 -0400

    Adds resolver for route module

--- src/app/adf-roles/adf-roles-routing.module.ts
+++ src/app/adf-roles/adf-roles-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DfManageRolesComponent } from './df-manage-roles/df-manage-roles.component';
 import { DfCreateRoleComponent } from './df-create-role/df-create-role.component';
+import { roleResolver } from './resolvers/role.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: DfManageRolesComponent,
+    resolve: { data: roleResolver },
   },
   {
     path: 'create',
