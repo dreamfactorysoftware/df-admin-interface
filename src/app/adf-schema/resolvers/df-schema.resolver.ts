941b542db648e3fe3ee9c76a9d98e11030ba0aae
William Henlin <william.henlin@rangle.io>
2023-09-11 17:02:09 -0400

    Refactor schema resolver to use crud service

--- src/app/adf-schema/resolvers/df-schema.resolver.ts
+++ src/app/adf-schema/resolvers/df-schema.resolver.ts
@@ -2,10 +2,10 @@ import { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
 import { GenericListResponse } from 'src/app/shared/types/generic-http.type';
 import { inject } from '@angular/core';
 import {
+  BASE_SERVICE_TOKEN,
   SERVICES_SERVICE_TOKEN,
   SERVICE_TYPES_SERVICE_TOKEN,
 } from '../../core/constants/tokens';
-import { DfDatabaseSchemaService } from '../services/df-database-schema.service';
 import { ServiceType } from 'src/app/shared/types/service';
 import { Service } from 'bonjour';
 
@@ -21,10 +21,11 @@ export const schemaServiceResolver: ResolveFn<
   return inject(SERVICES_SERVICE_TOKEN).getAll();
 };
 
-// TODO: change type here from any to appropriate type
-export const schemaResolver: ResolveFn<any> = (
-  route: ActivatedRouteSnapshot
-) => {
-  const name = route.paramMap.get('name') as string;
-  return inject(DfDatabaseSchemaService).getDatabaseSchemas(name);
+export const schemaResolver: ResolveFn<
+  GenericListResponse<Array<{ name: string; label: string }>>
+> = (route: ActivatedRouteSnapshot) => {
+  const dbName = route.paramMap.get('name') as string;
+  return inject(BASE_SERVICE_TOKEN).get(`${dbName}/_schema`, {
+    fields: ['name', 'label'].join(','),
+  });
 };
