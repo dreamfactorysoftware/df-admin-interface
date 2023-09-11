2e4f95d5ee79b550123b2680208788024e22c90d
Jordan Finch <jordan.finch@rangle.io>
2023-09-11 15:05:57 -0400

    implements table details post/patch

--- src/app/adf-schema/df-table-details/df-table-details.component.ts
+++ src/app/adf-schema/df-table-details/df-table-details.component.ts
@@ -1,8 +1,6 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import {
-  FormArray,
   FormBuilder,
-  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
@@ -10,7 +8,6 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
-import { uniqueNameValidator } from '../../shared/validators/unique-name.validator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TranslocoPipe } from '@ngneat/transloco';
@@ -45,6 +42,8 @@ export class DfTableDetailsComponent implements OnInit, OnDestroy {
   destroyed$ = new Subject<void>();
   type: string;
   dbName: string;
+  tableFields: [];
+  tableRelated: [];
 
   constructor(
     @Inject(BASE_SERVICE_TOKEN)
@@ -60,8 +59,6 @@ export class DfTableDetailsComponent implements OnInit, OnDestroy {
       label: [null],
       plural: [null],
       description: [null],
-      field: this.fb.array([], [uniqueNameValidator]),
-      related: this.fb.array([], [uniqueNameValidator]),
     });
   }
 
@@ -69,11 +66,22 @@ export class DfTableDetailsComponent implements OnInit, OnDestroy {
     this.activatedRoute.data
       .pipe(takeUntil(this.destroyed$))
       .subscribe(data => {
-        console.log('route data', data);
+        this.dbName = this.activatedRoute.snapshot.params['name'];
         this.type = data['type'];
-      });
 
-    this.dbName = this.activatedRoute.snapshot.params['name'];
+        if (this.type === 'edit') {
+          this.tableDetailsForm.patchValue({
+            name: data['data'].name,
+            alias: data['data'].alias,
+            label: data['data'].label,
+            plural: data['data'].plural,
+            description: data['data'].description,
+          });
+
+          this.tableFields = data['data'].field;
+          this.tableRelated = data['data'].related;
+        }
+      });
   }
 
   ngOnDestroy(): void {
@@ -82,42 +90,19 @@ export class DfTableDetailsComponent implements OnInit, OnDestroy {
   }
 
   goBack() {
-    this.router.navigate(['../'], {
-      relativeTo: this.activatedRoute,
-    });
+    if (this.type === 'create') {
+      this.router.navigate(['../'], {
+        relativeTo: this.activatedRoute,
+      });
+    } else if (this.type === 'edit') {
+      this.router.navigate(['../../'], {
+        relativeTo: this.activatedRoute,
+      });
+    }
   }
 
   save() {
     const data = this.tableDetailsForm.value;
-
-    // TODO: DELETE - needed while field component is in progress
-    data.field.push({
-      allow_null: false,
-      auto_increment: true,
-      db_function: null,
-      db_type: null,
-      default: null,
-      fixed_length: false,
-      is_aggregate: false,
-      is_foreign_key: false,
-      is_primary_key: false,
-      is_unique: false,
-      is_virtual: false,
-      label: 'id',
-      length: null,
-      name: 'id',
-      picklist: null,
-      precision: null,
-      ref_field: '',
-      ref_table: '',
-      required: false,
-      scale: 0,
-      supports_multibyte: false,
-      type: 'id',
-      validation: null,
-      value: [],
-      alias: 'id',
-    });
 
     const payload = {
       resource: [data],
@@ -132,6 +117,13 @@ export class DfTableDetailsComponent implements OnInit, OnDestroy {
           },
           `${this.dbName}/_schema`
         )
+        .pipe(takeUntil(this.destroyed$))
+        .subscribe(() => {
+          this.goBack();
+        });
+    } else if (this.type === 'edit') {
+      this.crudService
+        .patch(`${this.dbName}/_schema/${data.name}`, data)
         .pipe(takeUntil(this.destroyed$))
         .subscribe(() => {
           this.goBack();
