0290d9b73da5468a9078633700945f84c1e395ab
Jasjeet Mavi <42419193+judgemavi@users.noreply.github.com>
2023-09-18 17:14:24 -0400

    file viewer action override

--- src/app/adf-files/df-files/df-files-table.component.ts
+++ src/app/adf-files/df-files/df-files-table.component.ts
@@ -1,26 +1,21 @@
 import { Component, Inject } from '@angular/core';
-import { DfManageTableComponent } from '../../shared/components/df-manage-table/df-manage-table.component';
+import {
+  Actions,
+  DfManageTableComponent,
+  DfManageTableModules,
+} from '../../shared/components/df-manage-table/df-manage-table.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { DfBreakpointService } from '../../core/services/df-breakpoint.service';
-import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { AsyncPipe, NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatMenuModule } from '@angular/material/menu';
+import { TranslocoService } from '@ngneat/transloco';
+import { MatDialog } from '@angular/material/dialog';
 import { DfBaseCrudService } from 'src/app/core/services/df-base-crud.service';
 import {
   BASE_SERVICE_TOKEN,
   FILE_SERVICE_TOKEN,
 } from 'src/app/core/constants/tokens';
-import { takeUntil } from 'rxjs';
-import { ROUTES } from 'src/app/core/constants/routes';
 import { FileTableRow, FileResponse } from '../df-files.types';
+import { getFilterQuery } from 'src/app/shared/utilities/filter-queries';
+import { ROUTES } from 'src/app/core/constants/routes';
 
 @Component({
   selector: 'df-files-table',
@@ -30,21 +25,7 @@ import { FileTableRow, FileResponse } from '../df-files.types';
     '../../shared/components/df-manage-table/df-manage-table.component.scss',
   ],
   standalone: true,
-  imports: [
-    MatButtonModule,
-    FontAwesomeModule,
-    MatTableModule,
-    MatPaginatorModule,
-    TranslocoPipe,
-    AsyncPipe,
-    NgIf,
-    NgFor,
-    NgTemplateOutlet,
-    MatMenuModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDialogModule,
-  ],
+  imports: [DfManageTableModules],
 })
 export class DfFilesTableComponent extends DfManageTableComponent<any> {
   constructor(
@@ -53,18 +34,10 @@ export class DfFilesTableComponent extends DfManageTableComponent<any> {
     router: Router,
     activatedRoute: ActivatedRoute,
     liveAnnouncer: LiveAnnouncer,
-    breakpointService: DfBreakpointService,
     translateService: TranslocoService,
     dialog: MatDialog
   ) {
-    super(
-      router,
-      activatedRoute,
-      liveAnnouncer,
-      breakpointService,
-      translateService,
-      dialog
-    );
+    super(router, activatedRoute, liveAnnouncer, translateService, dialog);
   }
   override allowFilter = false;
   override allowCreate = false;
@@ -85,6 +58,19 @@ export class DfFilesTableComponent extends DfManageTableComponent<any> {
     },
   ];
 
+  override actions: Actions<any> = {
+    default: {
+      label: 'view',
+      function: (row: any) => {
+        this.router.navigate([ROUTES.ADMIN_SETTINGS, ROUTES.FILES, row.path]);
+      },
+      ariaLabel: {
+        key: 'view',
+      },
+    },
+    additional: this.actions.additional,
+  };
+
   mapDataToTable(data: any): FileTableRow[] {
     return data.map((app: FileResponse) => {
       return {
@@ -96,24 +82,12 @@ export class DfFilesTableComponent extends DfManageTableComponent<any> {
     });
   }
 
-  filterQuery(value: string): string {
-    return '';
-  }
-
-  override editRow(row: FileTableRow): void {
-    this.router.navigate([ROUTES.ADMIN_SETTINGS, ROUTES.FILES, row.path], {
-      queryParams: {
-        file: 'true',
-      },
-    });
-  }
+  filterQuery = getFilterQuery();
 
   override deleteRow(row: FileTableRow): void {
     // TODO: implement error handling
     //  this.triggerAlert
   }
 
-  refreshTable(limit?: number, offset?: number, filter?: string): void {
-    //TODO ??
-  }
+  refreshTable = () => null;
 }
