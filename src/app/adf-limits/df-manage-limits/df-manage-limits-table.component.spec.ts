e7744d0aaffc74e855de68b7f0fe663773275478
f1nch <f1nch@users.noreply.github.com>
2023-09-24 12:11:27 -0400

    Adds manage limits table tests (#56)
    Co-authored-by: Jordan Finch <jordan.finch@rangle.io>

---
+++ src/app/adf-limits/df-manage-limits/df-manage-limits-table.component.spec.ts
@@ -0,0 +1,70 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DfManageLimitsTableComponent } from './df-manage-limits-table.component';
+import { TranslocoService, provideTransloco } from '@ngneat/transloco';
+import { TranslocoHttpLoader } from '../../../transloco-loader';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  LIMIT_CACHE_SERVICE_PROVIDERS,
+  LIMIT_SERVICE_PROVIDERS,
+} from 'src/app/core/constants/providers';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { DfBaseCrudService } from '../../core/services/df-base-crud.service';
+
+describe('DfManageLimitsTableComponent', () => {
+  let component: DfManageLimitsTableComponent;
+  let fixture: ComponentFixture<DfManageLimitsTableComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        DfManageLimitsTableComponent,
+        HttpClientTestingModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        provideTransloco({
+          config: {
+            defaultLang: 'en',
+            availableLangs: ['en'],
+          },
+          loader: TranslocoHttpLoader,
+        }),
+        TranslocoService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            data: {
+              pipe: () => {
+                return {
+                  subscribe: (fn: (value: any) => void) => fn({}),
+                };
+              },
+            },
+          },
+        },
+        ...LIMIT_CACHE_SERVICE_PROVIDERS,
+        ...LIMIT_SERVICE_PROVIDERS,
+      ],
+    });
+    fixture = TestBed.createComponent(DfManageLimitsTableComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should refresh rows', () => {
+    const crudServiceSpy = jest.spyOn(DfBaseCrudService.prototype, 'delete');
+    component.refreshRows(2);
+    expect(crudServiceSpy).toHaveBeenCalled();
+  });
+
+  it('should throw error on refresh rows if there are no rows', () => {
+    const crudServiceSpy = jest.spyOn(DfBaseCrudService.prototype, 'delete');
+    component.refreshRows();
+    expect(crudServiceSpy).toThrowError();
+  });
+});
