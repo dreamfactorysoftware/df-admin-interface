a0f0b25be1e4fa081a7e01a5314a1d99f9af9e14
whenlin-rangle <122292164+whenlin-rangle@users.noreply.github.com>
2023-09-24 12:13:45 -0400

    Add role details tests (#47)
    * Add role details tests
    
    * Remove dead code
    
    * Update df-role-details.component.spec.ts

--- src/app/adf-roles/df-create-role/df-create-role.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'df-df-create-role',
-  templateUrl: './df-create-role.component.html',
-  styleUrls: ['./df-create-role.component.scss'],
-  standalone: true,
-})
-export class DfCreateRoleComponent {}
