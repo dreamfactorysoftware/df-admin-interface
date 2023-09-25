6b5b27f160faf49fe5ae25f7d9d9649e283aff52
William Henlin <william.henlin@rangle.io>
2023-09-25 11:11:18 -0400

    Add tests for profile component

--- src/app/adf-profile/df-profile/df-profile.component.spec.ts
+++ src/app/adf-profile/df-profile/df-profile.component.spec.ts
@@ -3,13 +3,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { provideTransloco, TranslocoService } from '@ngneat/transloco';
-import { USER_SERVICE_PROVIDERS } from '../../core/constants/providers';
-import { DfBreakpointService } from '../../core/services/df-breakpoint.service';
-import { DfSystemConfigDataService } from '../../core/services/df-system-config-data.service';
-import { TranslocoHttpLoader } from '../../../transloco-loader';
+import { USER_SERVICE_PROVIDERS } from 'src/app/core/constants/providers';
+import { DfBreakpointService } from 'src/app/core/services/df-breakpoint.service';
+import { DfSystemConfigDataService } from 'src/app/core/services/df-system-config-data.service';
+import { TranslocoHttpLoader } from 'src/transloco-loader';
 import { DfProfileComponent } from './df-profile.component';
 import { DfProfileService } from '../services/df-profile.service';
 import { UserProfile } from 'src/app/shared/types/user';
+import { DfPasswordService } from 'src/app/adf-user-management/services/df-password.service';
 
 const mockProfile: Partial<UserProfile> = {
   username: 'whenlin',
@@ -63,6 +64,7 @@ describe('DfProfileComponent', () => {
         DfSystemConfigDataService,
         DfBreakpointService,
         DfProfileService,
+        DfPasswordService,
         TranslocoService,
         {
           provide: ActivatedRoute,
@@ -133,5 +135,55 @@ describe('DfProfileComponent', () => {
     expect(component.profileForm.get('currentPassword')).toBeTruthy();
   });
 
-  // TODO: add tests for sequrity question form and change password form
+  it('security question is updated given the form input is valid and the save button was clicked', () => {
+    const profileSpy = jest.spyOn(DfProfileService.prototype, 'saveProfile');
+
+    component.securityQuestionForm.patchValue({
+      securityQuestion: 'What is your favorite drink?',
+      securityAnswer: 'water',
+    });
+
+    component.securityQuestionForm.markAsDirty();
+
+    component.updateSecurityQuestion();
+
+    expect(profileSpy).toHaveBeenCalled();
+  });
+
+  it('password is updated given the form input is valid and the save button was clicked', () => {
+    const passwordServiceSpy = jest.spyOn(
+      DfPasswordService.prototype,
+      'updatePassword'
+    );
+
+    component.updatePasswordForm.patchValue({
+      oldPassword: 'password',
+      newPassword: 'password1',
+      confirmPassword: 'password1',
+    });
+
+    component.updatePasswordForm.markAsDirty();
+
+    component.updatePassword();
+
+    expect(passwordServiceSpy).toHaveBeenCalled();
+  });
+
+  it('password is not updated given the form input is invalid and the save button was clicked', () => {
+    const passwordServiceSpy = jest.spyOn(
+      DfPasswordService.prototype,
+      'updatePassword'
+    );
+    component.updatePasswordForm.patchValue({
+      oldPassword: 'password',
+      newPassword: 'password1',
+      confirmPassword: 'password2',
+    });
+
+    component.updatePasswordForm.markAsDirty();
+
+    component.updatePassword();
+
+    expect(passwordServiceSpy).not.toHaveBeenCalled();
+  });
 });
