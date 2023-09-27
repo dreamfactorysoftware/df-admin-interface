20f400392cd2b73ee453144f5a2bc8fa1ff9b203
Jordan Finch <jordan.finch@rangle.io>
2023-09-27 11:09:03 -0400

    removes unused route import

--- src/app/shared/components/df-error/df-error.spec.ts
+++ src/app/shared/components/df-error/df-error.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DfErrorComponent } from './df-error.component';
 import { TranslocoService, provideTransloco } from '@ngneat/transloco';
 import { TranslocoHttpLoader } from 'src/transloco-loader';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
