0bf42678c43799657f5d8ad240b2c3e909e97ec0
Jasjeet Mavi <42419193+judgemavi@users.noreply.github.com>
2023-09-27 14:20:27 -0400

    jest module import patch

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!@angular|swagger-ui|react-syntax-highlighter|swagger-client|@ngneat)',
+  ],
   coverageReporters: ['html'],
   collectCoverageFrom: ['src/**/*.ts', '!src/index.ts', '!src/**/*.d.ts'],
 };
