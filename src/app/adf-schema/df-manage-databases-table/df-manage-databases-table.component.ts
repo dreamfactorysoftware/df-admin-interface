98c06e9d48e2e4dfa7857d0d4e4d229b5c5eab3b
whenlin-rangle <122292164+whenlin-rangle@users.noreply.github.com>
2023-09-29 13:46:35 -0400

    Update refreshTable fn in manage db table (#89)

--- src/app/adf-schema/df-manage-databases-table/df-manage-databases-table.component.ts
+++ src/app/adf-schema/df-manage-databases-table/df-manage-databases-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import {
   Actions,
   DfManageTableComponent,
@@ -9,9 +9,17 @@ import { TranslocoService } from '@ngneat/transloco';
 import { DatabaseRowData } from '../df-schema.types';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Service } from '../../shared/types/service';
+import { Service, ServiceType } from '../../shared/types/service';
 import { getFilterQuery } from 'src/app/shared/utilities/filter-queries';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import {
+  BASE_SERVICE_TOKEN,
+  SERVICES_SERVICE_TOKEN,
+  SERVICE_TYPE_SERVICE_TOKEN,
+} from 'src/app/shared/constants/tokens';
+import { DfBaseCrudService } from 'src/app/shared/services/df-base-crud.service';
+import { GenericListResponse } from 'src/app/shared/types/generic-http.type';
+import { forkJoin, map, switchMap } from 'rxjs';
 @UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'df-manage-databases-table',
@@ -25,6 +33,12 @@ import { UntilDestroy } from '@ngneat/until-destroy';
 })
 export class DfManageDatabasesTableComponent extends DfManageTableComponent<DatabaseRowData> {
   constructor(
+    @Inject(BASE_SERVICE_TOKEN)
+    private service: DfBaseCrudService,
+    @Inject(SERVICE_TYPE_SERVICE_TOKEN)
+    private serviceTypeService: DfBaseCrudService,
+    @Inject(SERVICES_SERVICE_TOKEN)
+    private servicesService: DfBaseCrudService,
     router: Router,
     activatedRoute: ActivatedRoute,
     liveAnnouncer: LiveAnnouncer,
@@ -95,13 +109,48 @@ export class DfManageDatabasesTableComponent extends DfManageTableComponent<Data
     });
   }
 
-  // TODO change refresh table
+  refreshTable(limit?: number, offset?: number, filter?: string): void {
+    const system = this._activatedRoute.snapshot.data['system'];
 
-  refreshTable(): void {
-    this._activatedRoute.data.subscribe(({ data }) => {
-      this.dataSource.data = this.mapDataToTable(data.resource);
-      this.tableLength = data.meta.count;
-    });
+    const groups = ['Database'];
+    const filteredDbGroups = groups.map(grp =>
+      this.serviceTypeService.getAll<GenericListResponse<ServiceType>>({
+        fields: 'name',
+        additionalParams: [
+          {
+            key: 'group',
+            value: grp,
+          },
+        ],
+      })
+    );
+
+    forkJoin(filteredDbGroups)
+      .pipe(
+        map(groups => groups.map(group => group.resource).flat()),
+        switchMap(serviceTypes => {
+          return this.servicesService
+            .getAll<GenericListResponse<Service>>({
+              limit,
+              sort: 'name',
+              filter: `${
+                system ? '(created_by_id is not null) and ' : ''
+              }(type in ("${serviceTypes.map(src => src.name).join('","')}"))${
+                filter ? ` and ${filter}` : ''
+              }`,
+            })
+            .pipe(
+              map(services => ({
+                ...services,
+                serviceTypes,
+              }))
+            );
+        })
+      )
+      .subscribe(data => {
+        this.dataSource.data = this.mapDataToTable(data.resource);
+        this.tableLength = data.meta.count;
+      });
   }
 
   filterQuery = getFilterQuery('services');
