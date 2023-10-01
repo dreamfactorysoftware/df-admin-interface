338693292d5df90a39825d0dab0c39e1bb2cc3f9
Jordan Finch <jordan.finch@rangle.io>
2023-10-01 13:05:19 -0400

    chore: adds more test fixes

--- src/app/shared/components/df-scripts-github-dialog/df-scripts-github-dialog.component.spec.ts
+++ src/app/shared/components/df-scripts-github-dialog/df-scripts-github-dialog.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DfScriptsGithubDialogComponent } from './df-scripts-github-dialog.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DfScriptsGithubDialogComponent', () => {
   let component: DfScriptsGithubDialogComponent;
   let fixture: ComponentFixture<DfScriptsGithubDialogComponent>;
+  let mockDialog: MatDialog;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DfScriptsGithubDialogComponent],
+      imports: [
+        DfScriptsGithubDialogComponent,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        { provide: MatDialog, useValue: mockDialog },
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+      ],
     });
     fixture = TestBed.createComponent(DfScriptsGithubDialogComponent);
     component = fixture.componentInstance;
