82320c6554b766a7ed1d24e30e9310c75d8cf03a
f1nch <f1nch@users.noreply.github.com>
2023-10-11 14:48:24 -0400

    fix/cors - adds success/error messages for create/update (#118)
    * adds success/error messages for create/update
    
    * chore: fixing create error path
    
    ---------
    
    Co-authored-by: Jordan Finch <jordan.finch@rangle.io>
    Co-authored-by: Jasjeet Mavi <42419193+judgemavi@users.noreply.github.com>

--- src/app/adf-config/df-cors/df-cors-config-details.component.ts
+++ src/app/adf-config/df-cors/df-cors-config-details.component.ts
@@ -14,11 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Router, ActivatedRoute } from '@angular/router';
-import {
-  TranslocoDirective,
-  TranslocoPipe,
-  TranslocoService,
-} from '@ngneat/transloco';
+import { TranslocoDirective, TranslocoPipe } from '@ngneat/transloco';
 import { CONFIG_CORS_SERVICE_TOKEN } from 'src/app/shared/constants/tokens';
 import { DfBaseCrudService } from 'src/app/shared/services/df-base-crud.service';
 import { CorsConfigData } from '../../shared/types/config';
@@ -26,9 +22,14 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { DfVerbPickerComponent } from 'src/app/shared/components/df-verb-picker/df-verb-picker.component';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import {
+  AlertType,
+  DfAlertComponent,
+} from 'src/app/shared/components/df-alert/df-alert.component';
+import {
   GenericCreateResponse,
   GenericUpdateResponse,
 } from 'src/app/shared/types/generic-http';
+import { catchError, throwError } from 'rxjs';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
@@ -50,6 +51,7 @@ import {
     TranslocoDirective,
     TranslocoPipe,
     DfVerbPickerComponent,
+    DfAlertComponent,
   ],
 })
 export class DfCorsConfigDetailsComponent implements OnInit {
@@ -57,16 +59,18 @@ export class DfCorsConfigDetailsComponent implements OnInit {
   corsConfigToEdit: CorsConfigData | undefined;
   allMethodsSelected = false;
   type = 'create';
+  alertMsg = '';
+  showAlert = false;
+  alertType: AlertType = 'error';
 
   constructor(
     @Inject(CONFIG_CORS_SERVICE_TOKEN)
     private corsConfigService: DfBaseCrudService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private translateService: TranslocoService,
     private formBuilder: FormBuilder
   ) {
-    this.corsForm = formBuilder.group({
+    this.corsForm = this.formBuilder.group({
       path: ['', Validators.required],
       description: [''],
       origins: ['', Validators.required],
@@ -100,6 +104,12 @@ export class DfCorsConfigDetailsComponent implements OnInit {
           this.allMethodsSelected = true;
       }
     });
+  }
+
+  triggerAlert(type: AlertType, msg: string) {
+    this.alertType = type;
+    this.alertMsg = msg;
+    this.showAlert = true;
   }
 
   private assemblePayload(): Partial<CorsConfigData> {
@@ -139,7 +149,17 @@ export class DfCorsConfigDetailsComponent implements OnInit {
             { resource: [payload] },
             {
               fields: '*',
+              snackbarSuccess: 'cors.alerts.createSuccess',
             }
+          )
+          .pipe(
+            catchError(err => {
+              this.triggerAlert(
+                'error',
+                err.error.error.context.resource[0].message
+              );
+              return throwError(() => new Error(err));
+            })
           )
           .subscribe(res => {
             this.router.navigate(['../', res.resource[0].id], {
@@ -152,7 +172,16 @@ export class DfCorsConfigDetailsComponent implements OnInit {
         this.corsConfigService
           .update<GenericUpdateResponse, Partial<CorsConfigData>>(
             this.corsConfigToEdit.id,
-            payload
+            payload,
+            {
+              snackbarSuccess: 'cors.alerts.updateSuccess',
+            }
+          )
+          .pipe(
+            catchError(err => {
+              this.triggerAlert('error', err.error.error.message);
+              return throwError(() => new Error(err));
+            })
           )
           .subscribe(res => {
             this.router.navigate(['../', res.id], {
