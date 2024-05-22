7cb4b6a668bb5ba610d3dbf1473e12c5d89ad4b8
Yi-Jacob <jacobyideveloper@gmail.com>
2024-05-22 12:21:15 -0500

    #208 - Refresh Cache bug and script issue fix

--- src/app/adf-config/df-cache/df-cache-table.component.ts
+++ src/app/adf-config/df-cache/df-cache-table.component.ts
@@ -1,5 +1,5 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { Component, Inject } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {
   DfManageTableComponent,
@@ -8,11 +8,17 @@ import {
 import { CacheRow, CacheType } from '../../shared/types/df-cache';
 import { DfBaseCrudService } from 'src/app/shared/services/df-base-crud.service';
 import { CACHE_SERVICE_TOKEN } from 'src/app/shared/constants/tokens';
-import { TranslocoService } from '@ngneat/transloco';
-import { MatDialog } from '@angular/material/dialog';
+import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
 import { faRefresh } from '@fortawesome/free-solid-svg-icons';
 import { getFilterQuery } from 'src/app/shared/utilities/filter-queries';
 import { Actions } from 'src/app/shared/types/table';
+import { MatButtonModule } from '@angular/material/button';
+import { DfPaywallComponent } from 'src/app/shared/components/df-paywall/df-paywall.component';
 
 @Component({
   selector: 'df-cache-table',
@@ -23,6 +29,7 @@ import { Actions } from 'src/app/shared/types/table';
   ],
   standalone: true,
   imports: DfManageTableModules,
+  providers: [DfBaseCrudService],
 })
 export class DfCacheTableComponent extends DfManageTableComponent<CacheRow> {
   constructor(
@@ -76,12 +83,59 @@ export class DfCacheTableComponent extends DfManageTableComponent<CacheRow> {
   }
 
   clearCache = (row: CacheRow) => {
-    this.cacheService
-      .delete(row.name, { snackbarSuccess: 'cache.serviceCacheFlushed' })
-      .subscribe();
+    this.openDialog(row);
+    // this.cacheService
+    //   .delete(row.name, { snackbarSuccess: 'cache.serviceCacheFlushed' })
+    //   .subscribe({
+    //     next: () => console.log('Cache flushed'),
+    //     error: (err: any) => console.error('Error flushing cache', err),
+    //   });
   };
+
+  openDialog(row: CacheRow) {
+    const dialogRef = this.dialog.open(DfCacheModal, {
+      data: { row },
+    });
+    dialogRef.afterClosed().subscribe();
+  }
 
   filterQuery = getFilterQuery();
 
   refreshTable = () => null;
+}
+
+@Component({
+  selector: 'df-cache-modal',
+  templateUrl: 'df-cache-modal.html',
+  styleUrls: ['./df-cache.component.scss'],
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    DfPaywallComponent,
+    TranslocoPipe,
+  ],
+})
+// eslint-disable-next-line @angular-eslint/component-class-suffix
+export class DfCacheModal {
+  @ViewChild('calendlyWidget') calendlyWidget: ElementRef;
+  row: CacheRow;
+  cacheService: DfBaseCrudService;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(CACHE_SERVICE_TOKEN)
+    cacheService: DfBaseCrudService
+  ) {
+    this.row = data.row;
+    this.cacheService = cacheService;
+  }
+
+  clearCache() {
+    this.cacheService
+      .delete(this.row.name, { snackbarSuccess: 'cache.serviceCacheFlushed' })
+      .subscribe({
+        next: () => console.log('Cache flushed'),
+        error: (err: any) => console.error('Error flushing cache', err),
+      });
+  }
 }
