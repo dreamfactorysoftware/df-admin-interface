6551ebcb0ccc084bd6e01d3fd6715674720a094c
Jason Gilmore <wj@wjgilmore.com>
2024-07-03 13:06:56 -0400

    Adding files modified by post-commit hook

--- src/transloco-loader.ts
+++ src/transloco-loader.ts
@@ -7,6 +7,8 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/dreamfactory/dist/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(
+      `/dreamfactory/dist/assets/i18n/${lang}.json`
+    );
   }
 }
