6932a61b1d8e8fd616cf5c0177496f3bceecd1a7
VitaliyHrabovych <vitaliihr@softwareplanet.group>
2025-05-15 15:09:41 +0300

    Add generate api card components

--- src/app/adf-home/df-welcome-page/df-welcome-page.component.ts
+++ src/app/adf-home/df-welcome-page/df-welcome-page.component.ts
@@ -38,6 +38,8 @@ import { DfBaseCrudService } from 'src/app/shared/services/df-base-crud.service'
 import { SERVICES_SERVICE_TOKEN } from 'src/app/shared/constants/tokens';
 import { DFStorageService } from 'src/app/shared/services/df-storage.service';
 import { ROUTES } from '../../shared/types/routes';
+import { DfGenerateApiCardComponent } from './df-generate-api-card/df-generate-api-card.component';
+
 @Component({
   selector: 'df-welcome-page',
   templateUrl: './df-welcome-page.component.html',
@@ -59,6 +61,7 @@ import { ROUTES } from '../../shared/types/routes';
     RouterModule,
     MatButtonModule,
     MatIconModule,
+    DfGenerateApiCardComponent,
   ],
   providers: [DfBaseCrudService],
 })
@@ -78,6 +81,8 @@ export class DfWelcomePageComponent {
   fpFileRoute = `/${ROUTES.API_CONNECTIONS}/${ROUTES.API_TYPES}/${ROUTES.FILE}/${ROUTES.CREATE}`;
   fpUtilityRoute = `/${ROUTES.API_CONNECTIONS}/${ROUTES.API_TYPES}/${ROUTES.UTILITY}/${ROUTES.CREATE}`;
 
+  public generateApiCardsData: any[];
+
   welcomePageResources = welcomePageResources;
   nativeExampleLinks = nativeExampleLinks;
   javaScriptExampleLinks = javaScriptExampleLinks;
@@ -87,7 +92,58 @@ export class DfWelcomePageComponent {
     private themeService: DfThemeService,
     private storageService: DFStorageService,
     @Inject(SERVICES_SERVICE_TOKEN) private servicesService: DfBaseCrudService
-  ) {}
+  ) {
+    this.generateApiCardsData = [
+      {
+        icon: this.faDatabase,
+        headerTextKey: 'home.welcomePage.connectToDatabaseCard.header',
+        textKey: 'home.welcomePage.connectToDatabaseCard.description',
+        route: this.fpDBRoute,
+        bgColor: 'rgba(127, 17, 224, 0.1)',
+        headerColor: 'rgb(127, 17, 224)',
+      },
+      {
+        icon: this.faCode,
+        headerTextKey: 'home.welcomePage.useScriptingServicesCard.header',
+        textKey: 'home.welcomePage.useScriptingServicesCard.description',
+        route: this.fpScriptingRoute,
+        bgColor: 'rgba(92,35,154, 0.1)',
+        headerColor: 'rgb(92,35,154)',
+      },
+      {
+        icon: this.faNetworkWired,
+        headerTextKey: 'home.welcomePage.proxyWebServicesCard.header',
+        textKey: 'home.welcomePage.proxyWebServicesCard.description',
+        route: this.fpNetworkRoute,
+        bgColor: 'rgb(235,253,245)',
+        headerColor: 'rgb(25,97,80)',
+      },
+      {
+        icon: this.faFile,
+        headerTextKey: 'home.welcomePage.exposeFileStorageCard.header',
+        textKey: 'home.welcomePage.exposeFileStorageCard.description',
+        route: this.fpFileRoute,
+        bgColor: 'rgb(255,251,236)',
+        headerColor: 'rgb(136,72,43)',
+      },
+      {
+        icon: this.faTools,
+        headerTextKey: 'home.welcomePage.utilityServicesCard.header',
+        textKey: 'home.welcomePage.utilityServicesCard.description',
+        route: this.fpUtilityRoute,
+        bgColor: 'rgba(80,105,137, 0.1)',
+        headerColor: 'rgb(80,105,137)',
+      },
+      {
+        icon: this.faTools,
+        headerTextKey: 'home.welcomePage.utilityServicesCard.header',
+        textKey: 'home.welcomePage.utilityServicesCard.description',
+        route: this.fpUtilityRoute,
+        bgColor: 'rgba(80,105,137, 0.1)',
+        headerColor: 'rgb(80,105,137)',
+      },
+    ];
+  }
   isDarkMode = this.themeService.darkMode$;
   isFirstTimeUser$ = this.storageService.isFirstTimeUser$;
   releases: any[] = [];
