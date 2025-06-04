86f2a7c6199be7210a26ba9d04ab9e46eaf8f449
Kevin McGahey <36458555+thekevinm@users.noreply.github.com>
2025-06-03 17:44:58 -0700

    Delete file: src/app/adf-profile/resolvers/profile.resolver.ts

--- src/app/adf-profile/resolvers/profile.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { ResolveFn } from '@angular/router';
-import { DfProfileService } from '../services/df-profile.service';
-import { inject } from '@angular/core';
-import { UserProfile } from '../../shared/types/user';
-
-export const profileResolver: ResolveFn<UserProfile> = () => {
-  const profileService = inject(DfProfileService);
-  return profileService.getProfile();
-};
