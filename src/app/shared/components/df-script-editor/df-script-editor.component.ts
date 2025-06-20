4d259691bf912d5cbf74bbbb316ee9bdbe9ad1ce
nicdavidson <ndavidson1717@gmail.com>
2025-06-20 12:34:23 -0600

    rebuild dist

--- src/app/shared/components/df-script-editor/df-script-editor.component.ts
+++ src/app/shared/components/df-script-editor/df-script-editor.component.ts
@@ -122,11 +122,9 @@ export class DfScriptEditorComponent implements OnInit {
   }
 
   viewLatest() {
-    const filePath = `${
-      this.storageServices.find(
-        service => service.id === this.storageServiceId.getRawValue()
-      )?.name
-    }/${this.storagePath.getRawValue()}`;
+    const filePath = `${this.storageServices.find(
+      service => service.id === this.storageServiceId.getRawValue()
+    )?.name}/${this.storagePath.getRawValue()}`;
     if (filePath.endsWith('.json')) {
       this.fileService
         .downloadJson(filePath)
