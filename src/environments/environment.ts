c299bfad3f0c0484092f29e8ab193b7a185f4373
Kevin McGahey <36458555+thekevinm@users.noreply.github.com>
2025-06-29 16:53:45 -0700

    Update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: false,
   dfAdminApiKey: '',
   dfApiDocsApiKey: '',
-  dfFileManagerApiKey: '',
-  builderApiKey: '91ab2f932895468888be9adb6a3c0b5d',
+  dfFileManagerApiKey: ''
 };
